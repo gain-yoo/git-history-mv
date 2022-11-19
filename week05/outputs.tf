f4cd8b440113e129e5ff1150be1b7340cdc7cb36
gain-yoo <100563973+gain-yoo@users.noreply.github.com>
2022-11-19 18:44:31 +0900

    Week 05 - 04_for_each

--- week05/outputs.tf
+++ week05/outputs.tf
@@ -1,9 +1,3 @@
-output "first_arn" {
-  value       = aws_iam_user.myiam[0].arn
-  description = "The ARN for the first user"
-}
-
-output "all_arns" {
-  value       = aws_iam_user.myiam[*].arn
-  description = "The ARNs for all users"
+output "all_users" {
+  value = values(aws_iam_user.myiam)[*].arn
 }
